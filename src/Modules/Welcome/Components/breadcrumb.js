a44ed3fb0e3cf78bd24036c989eaa8f5ddc1d349
smith sese <samoa@javolin.com>
2023-04-01 14:23:00 +0000

    carousel addition

--- src/Modules/Welcome/Components/breadcrumb.js
+++ src/Modules/Welcome/Components/breadcrumb.js
@@ -1,13 +1,34 @@
 import LockIcon from "../../../Shared/Component/Icons/LockIcon";
 import EyeIcon from "../../../Shared/Component/Icons/EyeIcon";
 import CashIcon from "../../../Shared/Component/Icons/CashIcon";
+import {useEffect, useState} from "react";
 
 export default function BreadCrumb() {
+
+    const images = [
+        'https://combo.staticflickr.com/ap/build/images/sohp/2020-top-25/Mists_of_renfrew_Adam_Gibbs.jpg',
+        'https://images.unsplash.com/photo-1679652557788-a6add7fcd992?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+        'https://images.unsplash.com/photo-1679613753438-125c62c94b48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80',
+    ];
+
+    const [currentImage, setCurrentImage] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentImage((currentImage + 1) % images.length);
+        }, 2000);
+
+        return () => clearInterval(intervalId);
+    }, [currentImage]);
+
+
+
+
     return (
         <div style={{height: '600px'}} className={"bg-gray-200 flex flex-row items-center px-12"}>
             <img
-                src={"https://combo.staticflickr.com/ap/build/images/sohp/2020-top-25/Mists_of_renfrew_Adam_Gibbs.jpg"}
-                className={"h-5/6 w-3/5 object-cover rounded"}
+                src={images[currentImage]}
+                className={"h-5/6 w-3/5 object-cover transition-transform duration-500 ease-in-out rounded"}
             />
             <div className={"px-20"}>
                 <h2 className={"font-rubik text-xl text-gray-900"}>Featured Editor Pick</h2>
