241f6d0a1f0a2ce1e15464d19a16e2e20c270c41
mrnabdulai <mryi1245@gmail.com>
2023-10-25 16:21:19 +0000

    uppercased only text

--- src/Modules/Welcome/BannerText.js
+++ src/Modules/Welcome/BannerText.js
@@ -5,7 +5,7 @@ function BannerText() {
         <div className="px-10 mb-4">
             <div className=' bg-red-600 text-center py-2  w-full'>
                 {/* #FE0000 */}
-                <h4 className='text-white ld:text-2xl md:text-xl text-base font-medium xl:w-2/4 lg:w-2/3 md:h-3/4 w-[90%]  tracking-wide leading-snug text-center mx-auto '>BIATHlE/TRIATHE PICTURES ARE ONLy ACCESSIBLE USING THE LINKS SHARED AFTER EACH DAY WITH THE HEAD OF DELEGATION</h4>
+                <h4 className='text-white ld:text-2xl md:text-xl text-base font-medium xl:w-2/4 lg:w-2/3 md:h-3/4 w-[90%]  tracking-wide leading-snug text-center mx-auto '>BIATHlE/TRIATHE PICTURES ARE ONLY ACCESSIBLE USING THE LINKS SHARED AFTER EACH DAY WITH THE HEAD OF DELEGATION</h4>
             </div>
         </div>
     )
