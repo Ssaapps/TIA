58816997c68894e1a10a51912fa8b3f9ca7fe064
mrnabdulai <mryi1245@gmail.com>
2023-10-25 16:17:38 +0000

    Resolved UI changes

---
+++ src/Modules/Welcome/BannerText.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function BannerText() {
+    return (
+        <div className="px-10 mb-4">
+            <div className=' bg-red-600 text-center py-2  w-full'>
+                {/* #FE0000 */}
+                <h4 className='text-white ld:text-2xl md:text-xl text-base font-medium xl:w-2/4 lg:w-2/3 md:h-3/4 w-[90%]  tracking-wide leading-snug text-center mx-auto '>BIATHlE/TRIATHE PICTURES ARE ONLy ACCESSIBLE USING THE LINKS SHARED AFTER EACH DAY WITH THE HEAD OF DELEGATION</h4>
+            </div>
+        </div>
+    )
+}
+
+export default BannerText
